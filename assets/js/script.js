ad02f945fc8c81aa324d22ef6b7172fe4f7eebd5
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-01-13 02:12:49 +1100

    Select pokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//API
+var LIST_API = "https://pokeapi.co/api/v2/pokemon?limit=151";
+
 // DOM Elements
 var buttonsContainer = document.getElementById('buttonsContainer');
 var inst = document.getElementById('instructions');
@@ -10,6 +13,7 @@ var playBtn1 = document.getElementById('playBtn1');
 var muteBtn1 = document.getElementById('muteBtn1');
 var playBtn2 = document.getElementById('playBtn2');
 var muteBtn2 = document.getElementById('muteBtn2');
+let pokemonList1 = document.getElementById("pokemonList1");
 
 // Radio buttons and forms
 var pvpRadio = document.getElementById('pvp');
@@ -19,12 +23,16 @@ var playerVsPlayerForm = document.getElementById('playerVsPlayerForm');
 var playerName = document.getElementById('playerName');
 var player1Name = document.getElementById('player1Name');
 var player2Name = document.getElementById('player2Name');
+var playerSel1 = document.getElementById('playerSel1');
+var playerSel2 = document.getElementById('playerSel2');
 
 // Pages
 var page1 = document.getElementById('page1');
 var page2 = document.getElementById('page2');
 var page3 = document.getElementById('page3');
 var page4 = document.getElementById('page4');
+var page5 = document.getElementById('page5');
+var page6 = document.getElementById('page6');
 
 var audioElement = document.querySelector('audio');
 
@@ -45,6 +53,8 @@ var instArray = [
 
 // Index to keep track of the current instruction
 var currentIndex = 0;
+var pokemonListArr = [];
+var state = 0;
 
 // Event Listeners
 playBtn1.addEventListener('click', toggleAudio);
@@ -56,7 +66,9 @@ instNextBtn.addEventListener('click', showNextInstruction);
 startGameBtn.addEventListener('click', startGame);
 backBtn.addEventListener('click', navigateBack);
 nextBtn.addEventListener('click', navigateNext);
-
+playerSel1.addEventListener('keyup', onKeyUp);
+playerSel2.addEventListener('keyup', onKeyUp);
+document.addEventListener("click", hideDropDown);
 // Function to play audio
 function playAudio() {
     audioElement.play();
@@ -126,28 +138,115 @@ function resetForm(form) {
     form.reset();
 }
 
-// Function to save player names based on the current page
-function savePlayerNames() {
-    if (!page3.classList.contains('hidden')) {
-        playerName.value;
-    } else if (!page4.classList.contains('hidden')) {
-        player1Name.value;
-        player2Name.value;
+// Function to navigate to the next page
+function navigateNext(event) {
+    if(state === 0){
+        if (pvcRadio.checked) {
+            page2.classList.add('hidden');
+            page3.classList.remove('hidden');
+            page4.classList.add('hidden');
+            if(playerName.value != "") {
+                mode = {
+                    option : 2,
+                    player1Name: playerName.value,
+                    player2Name: null
+                }
+                page3.classList.add('hidden');
+                page5.classList.remove('hidden');
+                searchApi();
+                state = 4; /* Page 4 Done ! */
+            }
+        } else if (pvpRadio.checked) {
+            page2.classList.add('hidden');
+            page3.classList.add('hidden');
+            page4.classList.remove('hidden');
+            if(player1Name.value != "" && player2Name.value != "") {
+                mode = {
+                    option : 1,
+                    player1Name: player1Name.value,
+                    player2Name: player2Name.value,
+                }
+                page4.classList.add('hidden');
+                page5.classList.remove('hidden');
+                searchApi();
+                state = 4; /* Page 4 Done ! */
+            }
+        } else {
+            console.log('No radio button selected');
+        }
+    }
+    console.log(state);
+    
+    if(state === 5) {
+        page5.classList.add('hidden');
+        page6.classList.remove('hidden');
     }
 }
 
-// Function to navigate to the next page
-function navigateNext() {
-    savePlayerNames();
-    if (pvcRadio.checked) {
-        page2.classList.add('hidden');
-        page3.classList.remove('hidden');
-        page4.classList.add('hidden');
-    } else if (pvpRadio.checked) {
-        page2.classList.add('hidden');
-        page3.classList.add('hidden');
-        page4.classList.remove('hidden');
-    } else {
-        console.log('No radio button selected');
-    }
+function searchApi() {
+    
+    var locQueryUrl = LIST_API;
+
+    fetch(locQueryUrl)
+      .then(function (response) {
+        if (!response.ok) {
+          throw response.json();
+        }
+        return response.json();
+      })
+      .then(function (locRes) {
+        if (!locRes.results.length) {
+          console.log('No results found!');
+        } else {
+            pokemonListArr = locRes.results;
+            renderOptions(pokemonListArr, 1);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+    });
+}
+
+function renderOptions(arr, option) {
+    let newHtml = ``;
+
+    arr.forEach((pokemon) => {
+        newHtml += `<div
+        onclick="selectOption${option}('${pokemon.name}')"
+        class="px-5 py-3 border-b border-gray-200 text-stone-600 cursor-pointer hover:bg-slate-100 transition-colors"
+        >
+        ${pokemon.name}
+        </div>`;
+    });
+    pokemonList1.innerHTML = newHtml;
+    pokemonList2.innerHTML = newHtml;
+}
+
+function onKeyUp(e) {
+    var idSelect = event.target.id.substring(e.target.id.length - 1);
+    console.log(idSelect);
+    console.log(typeof idSelect);
+    let keyword = e.target.value;
+    idSelect === "1" ? pokemonList1.classList.remove("hidden"): pokemonList2.classList.remove("hidden");
+    let filteredPokemons = pokemonListArr.filter((c) =>
+      c.name.toLowerCase().includes(keyword.toLowerCase())
+    );
+    renderOptions(filteredPokemons, idSelect);
+}
+
+function selectOption1(selectedOption) {
+    hideDropDown();
+    playerSel1.value = selectedOption;
+    state = 5; /* Page 5 Done ! */
+}
+
+function selectOption2(selectedOption) {
+    hideDropDown();
+    playerSel2.value = selectedOption;
+    state = 6; /* Page 6 Done ! */
+}
+
+function hideDropDown() {
+    pokemonList1.classList.add("hidden");
+    pokemonList2.classList.add("hidden");
 }
